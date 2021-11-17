856000e96b84e3d3e23bf2919c5adc9d046cd2e5
Scott Rohrig <scott.rohrig@gmail.com>
2021-11-17 15:07:24 -0800

    MVP-complete

--- script.js
+++ script.js
@@ -1,14 +1,86 @@
-// Assignment Code
+// Store Reference to Required Elements On Start
 var generateBtn = document.querySelector("#generate");
+var createPwBtn = document.querySelector("#create");
+var checkboxes = document.querySelectorAll(".checkbox");
+var pwLengthEl = document.querySelector('#charCountSlider');
+var form = document.querySelector('.form-container');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Define constants
+const lowers = 'abcdefghijklmnopqrstuvwxyz';
+const capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numerals = '0123456789';
+const symbols = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
+const kinds = [lowers,capitals,numerals,symbols];
 
-  passwordText.value = password;
+/** handles open/cancel state of the form element */
+const handleFormToggle = () => {
+  var formWrapper = document.querySelector("#pw-criteria-form");
+  var closed = formWrapper.style.display !== "block";
+  if (closed) {
+    // TODO: change generateBtn style by adding a 'cancel' class
+    formWrapper.style.display = "block";
+  } else {
+    // TODO: change generateBtn style by removing the 'cancel' class
+    formWrapper.style.display = "none";
+  }
+};
 
+// Add event listener to buttons
+generateBtn.addEventListener("click", handleFormToggle);
+createPwBtn.addEventListener("click", writePassword);
+
+/** Returns an element at a random index from a given iterable */
+const getRandomChar = (charType) => {
+  let char = Math.floor(Math.random() * charType.length);
+  return charType[char]
+};
+
+/** Returns a String of character combining character types 
+ * based on the forms selected checkboxes */
+function makeCharacterSet() {
+  var selected = [];
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      selected[i] = kinds[i];
+    }
+  }
+  if (!selected.length) {
+    return '';
+  }
+  return selected.join('');
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+/** Returns a string of random characters */
+const generatePassword = () => {
+  
+  // Retrieve the desired character length of the password
+  var pwLength = pwLengthEl.value;
+  // Initialize empty array to hold new values
+  var pwArray = [];
+  
+  // Combine the selected character types into a single string
+  var selected = makeCharacterSet();
+  
+  if (selected) {
+    // Iterate thru desired pw length
+    for (let i = 0; i < pwLength; i++ ) {
+      
+      // select random letter
+      var character = getRandomChar(selected);
+      
+      // add character to pw
+      pwArray.push(character);
+    }
+  }
+
+  var password = pwArray.join('');
+  return password
+};
+
+// Write password to the #password input
+function writePassword(e) {
+  e.preventDefault();
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
